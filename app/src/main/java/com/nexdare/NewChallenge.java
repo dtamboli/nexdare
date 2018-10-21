f2876f08c847a7a87fa704bbe38d18a216f6565c
Nikhil Kupireddy <Nikhil.Kupireddy@bhnetwork.com>
2018-10-21 15:18:38 -0400

    new challenge

--- app/src/main/java/com/nexdare/NewChallenge.java
+++ app/src/main/java/com/nexdare/NewChallenge.java
@@ -15,19 +15,30 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.nexdare.models.Challenge;
 import com.nexdare.utility.UniversalImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.joda.time.Hours;
 import org.joda.time.LocalDateTime;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static java.lang.Integer.parseInt;
 
@@ -83,6 +94,8 @@ public class NewChallenge extends AppCompatActivity {
         // Create the choose date/time button
         createButtonChooseDateTime();
 
+        createChallengeButton();
+
     }
 
     @Override
@@ -122,8 +135,7 @@ public class NewChallenge extends AppCompatActivity {
     }
 
     public void init(){
-     //   createChallenge();
-      //  getChallenges();
+        createChallenge();
     }
 
     private void createTextViewDateTime() {
@@ -140,6 +152,32 @@ public class NewChallenge extends AppCompatActivity {
     private String formatDateString(LocalDateTime localDateTime) {
         return localDateTime.toString("MM/dd/yyyy hh:mm a");
     }
+
+    private void createChallengeButton() {
+        Button button = (Button)safeFindViewById(R.id.Send);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+              createChallenge();
+                Intent intent = new Intent(NewChallenge.this, ListChallenges.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+
+    private View safeFindViewById(int viewId) {
+        final String METHOD = "createButtonChooseDateTime(" + viewId + ")";
+        View ret = findViewById(viewId);
+        if (ret == null) {
+            Log.e(TAG, METHOD + "Something has gone horribly wrong.");
+        }
+        return ret;
+    }
+
 
     private void createButtonChooseDateTime() {
         Button button = (Button)safeFindViewById(R.id.button_choosedatetime);
@@ -163,43 +201,36 @@ public class NewChallenge extends AppCompatActivity {
         });
     }
 
-
-    private View safeFindViewById(int viewId) {
-        final String METHOD = "createButtonChooseDateTime(" + viewId + ")";
-        View ret = findViewById(viewId);
-        if (ret == null) {
-            Log.e(TAG, METHOD + "Something has gone horribly wrong.");
-        }
-        return ret;
-    }
-
-
     private void createChallenge() {
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
         //get a database reference
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference()
                 .child(getString(R.string.dbnode_challenges));
 
         //create the new messages id
         String newMessageId = reference.push().getKey();
+
         Challenge challenge = new Challenge();
-        challenge.setChallenge_id("Id");
-        challenge.setName("10 PushUps");
-        challenge.setStatus("ACCEPTED");
-        challenge.setSentTo("Nikhil");
+        challenge.setChallenge_id(newMessageId);
+        challenge.setName(challengeName.getText().toString());
+
+     //   challenge.setName();
+        challenge.setStatus("CREATED");
+        challenge.setSentTo(to.getText().toString());
         challenge.setUserId(user.getEmail());
-        challenge.setRules("Post Video as proof");
-        challenge.setTimeFrame(100);
+        challenge.setRules(rules.getText().toString());
+
+        LocalDateTime currentTime = new LocalDateTime();
+        int hours  = Hours.hoursBetween(currentTime.toLocalDate(), mLocalDateTime.toLocalDate()).getHours();
+
+        challenge.setTimeFrame(hours);
 
         //insert the new message into the chatroom
         reference
                 .child(newMessageId)
                 .setValue(challenge);
     }
-
-
 
 
     @Override
@@ -237,6 +268,11 @@ public class NewChallenge extends AppCompatActivity {
                 intent = new Intent(NewChallenge.this, ChatActivity.class);
                 startActivity(intent);
                 return true;
+            case R.id.optionAdmin:
+                    intent = new Intent(NewChallenge.this, AdminActivity.class);
+                    startActivity(intent);
+                return true;
+
             default:
                 return super.onOptionsItemSelected(item);
         }
